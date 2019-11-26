561b5f1b58ec464482fb727accc7874610ae7bef
vitalcha <vkrokhov@gmail.com>
2019-11-26 17:02:11 +0200

    Updates

---
+++ precache-manifest.1e586d72e18e55f43d14fe1f41b72057.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e59e3e6ef3b1c22a3da3ceca4f7058a",
+    "url": "/goit-react-hw-02-reader/index.html"
+  },
+  {
+    "revision": "9ef2f32131e5c8e99b4a",
+    "url": "/goit-react-hw-02-reader/static/css/main.94f7c198.chunk.css"
+  },
+  {
+    "revision": "23f9366c5ee0ac796f71",
+    "url": "/goit-react-hw-02-reader/static/js/2.64650066.chunk.js"
+  },
+  {
+    "revision": "9ef2f32131e5c8e99b4a",
+    "url": "/goit-react-hw-02-reader/static/js/main.6e02d60e.chunk.js"
+  },
+  {
+    "revision": "299f48f5aa6019a15bd5",
+    "url": "/goit-react-hw-02-reader/static/js/runtime-main.ef9e362e.js"
+  }
+]);
